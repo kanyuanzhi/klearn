acd29369ea8a997d08ab17e4324816937c6366c1
kanyuanzhi <kan@mail.ustc.edu.cn>
2018-04-28 02:24:53 +0800

    finish kdTree

--- kan_ml/knn.py
+++ kan_ml/knn.py
@@ -48,7 +48,10 @@ class KNN(object):
         # print distance_list
         # print distance_list[3]
         k_distance = distance_list[0:self.k]
-        # print k_distance
+        print k_distance
+        for k in k_distance:
+            print self.points[k[0]]
+        print '\t'
         k_tags = [self.tags[kd[0]] for kd in k_distance]
         tag_count = {}  # 每个标签有多少个
         for kt in k_tags:
@@ -64,36 +67,103 @@ class KNN(object):
     def __train_by_kdtree(self):
         candidate_point = []
         kdtree = KDTree(self.points)
-        kdtree.scan_tree()
+        # kdtree.scan_tree()
 
         current_node = self.find_leaf(kdtree.root, self.test_point)
-        current_node.visited = True
         # close_in_distance = KNN.__distance(close_in_node.value['coordinate'], self.test_point)
         # candidate_point.append({'node': close_in_node, 'distance': close_in_distance})
         while True:
+            """
+            算法描述：
+            
+            如果candidate_point长度不等于k，则判断current_node是否访问过：
+            若访问过，则一直爬到其未被访问过的父节点，
+                若爬到父节点为根节点，若访问过，结束，若没有访问过，比较candidate_point的最大距离是否大于test_point到current_node切分线
+                的垂直距离，若大于，进入另一侧，找到叶节点，重复，若不大于，结束，
+                若不是父节点，重复；
+            若没有访问过，则将current_node加入候选序列，然后比较candidate_point的最大距离是否大于test_point到current_node切分线的垂直距离，若大于，
+            进入current另一侧找到叶节点，重复，若不大于，则一直爬到其未被访问过的父节点，若爬到父节点为根节点，若访问过，结束，若没有访问过，
+            比较candidate_point的最大距离是否大于test_point到current_node切分线的垂直距离，若大于，进入另一侧，找到叶节点，重复，
+            若不大于，结束；
+            
+            如果candidate_point长度等于k，则判断current_node是否访问过：
+            若访问过，则一直爬到其未被访问过的父节点，
+                若爬到父节点为根节点，若访问过，结束，若没有访问过，比较candidate_point的最大距离是否大于test_point到current_node切分线
+                的垂直距离，若大于，进入另一侧，找到叶节点，重复，若不大于，结束，
+                若不是父节点，重复；
+            若没有访问过，则比较distance(current_node,test_node)是否大于candidate_point中最大距离，若小于，则替换掉candidate_point中的最大距离，
+            然后比较candidate_point的最大距离是否大于test_point到current_node切分线的垂直距离，若大于，则进入另一侧找到叶节点，重复，
+            若不大于，则一直爬到其为被访问过的父节点，若该父节点为根节点，若访问过，结束，若没有访问过，比较candidate_point的最大距离
+            是否大于test_point到current_node切分线的垂直距离，若大于，进入另一侧，找到叶节点，重复，若不大于，结束；
+            """
+            while current_node.visited:
+                if current_node.parent is None:
+                    break
+                previous_node = current_node
+                current_node = current_node.parent
+
+            if current_node.parent is None:
+                if current_node.visited:
+                    break
+
             if len(candidate_point) != self.k:
                 current_distance = KNN.__distance(current_node.value['coordinate'], self.test_point)
                 current_node.visited = True
                 candidate_point.append({'node': current_node, 'distance': current_distance})
-                if current_node.parent is not None:
-                    current_node = current_node.parent
+                if current_node.left is None:
+                    continue
                 else:
                     if self.intersect_check(candidate_point, current_node):
-                        pass
+                        location = previous_node.location
+                        if location == 'left':
+                            if current_node.right is None:
+                                continue
+                            else:
+                                current_node = self.find_leaf(current_node.right, self.test_point)
+                                continue
+                        if location == 'right':
+                            if current_node.left is None:
+                                continue
+                            else:
+                                current_node = self.find_leaf(current_node.left, self.test_point)
                     else:
-                        break
+                        continue
             else:
-                current_distance = KNN.__distance(current_node.value['coordinate', self.test_point])
+                current_distance = KNN.__distance(current_node.value['coordinate'], self.test_point)
                 candidate_point.sort(key=lambda x: x['distance'])
                 if current_distance < candidate_point[self.k - 1]['distance']:
+                    current_node.visited = True
                     candidate_point.pop()
                     candidate_point.append({'node': current_node, 'distance': current_distance})
+                else:
+                    current_node.visited = True
+                if current_node.left is None:
+                    continue
+                else:
+                    if self.intersect_check(candidate_point, current_node):
+                        location = previous_node.location
+                        if location == 'left':
+                            if current_node.right is None:
+                                continue
+                            else:
+                                current_node = self.find_leaf(current_node.right, self.test_point)
+                                continue
+                        if location == 'right':
+                            if current_node.left is None:
+                                continue
+                            else:
+                                current_node = self.find_leaf(current_node.left, self.test_point)
+                    else:
+                        continue
+
+        for cd in candidate_point:
+            print cd['node'].value['coordinate']
 
     def intersect_check(self, candidate_point, current_node):
         candidate_point.sort(key=lambda x: (x['distance']))
         d = current_node.value['dimension']
         if candidate_point[len(candidate_point) - 1]['distance'] >= np.abs(
-                        self.points[d] - current_node.value['coordinate'][d]):
+                        self.test_point[d] - current_node.value['coordinate'][d]):
             return True
         else:
             return False
@@ -176,19 +246,19 @@ class KNN(object):
 
 
 if __name__ == "__main__":
-    n = 500
+    n = 5000
     np.random.seed(10)
     x = np.random.randint(0, 200, size=(3, n))
     y = np.random.randint(0, 3, size=(1, n))
     # print x.T
     # print y
-    test_point = [3, 4, 50]
-    # knn = KNN(x.T, y[0], test_point, 5, False)
+    test_point = [3, 4,50]
+    knn = KNN(x.T, y[0], test_point, 5, False)
     # tag = knn.get_result()
     # print "predicted tag:", tag
-    xx = np.array([(2, 5, 9, 4, 8, 7), (3, 4, 6, 7, 1, 2)])
-    yy = np.random.randint(0, 2, size=(1, 6))
-    test_pointt = [4, 3]
-    knn = KNN(xx.T, yy[0], test_pointt, 5, False)
-    tag = knn.get_result()
+    # xx = np.array([(2, 5, 9, 4, 8, 7), (3, 4, 6, 7, 1, 2)])
+    # yy = np.random.randint(0, 2, size=(1, 6))
+    # test_pointt = [4, 3]
+    # knn = KNN(xx.T, yy[0], test_pointt, 5, False)
+    # tag = knn.get_result()
     # ktree = KDTree(xx.T)
