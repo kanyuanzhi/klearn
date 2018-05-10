2bb4f2045e79eb56dc1861ae4434f8cd282a7535
kanyuanzhi <kan@mail.ustc.edu.cn>
2018-05-10 22:15:47 +0800

    modify the parameters reference

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='kan_ml',
-    version='0.0.1',
+    version='0.0.2',
     keywords='machine-learning',
     description='machine learning tools',
     url='https://github.com/kanyuanzhi/kan_ML',
